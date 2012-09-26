5df0ade7b032cd022d2281a44d4d45f15c200203
HOSOKAWA Jun <freeonterminate@gmail.com>
2012-09-26 15:04:35 +0900

    DDASample Project Added

---
+++ DDASample/Project1.dpr
@@ -0,0 +1,14 @@
+program Project1;
+
+uses
+  FMX.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  uDDASample in 'uDDASample.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
