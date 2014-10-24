7aedc30ae4402b7087e07aaf023431dd1883ebbb
HOSOKAWA Jun <jun@2nd5.com>
2014-10-24 23:47:07 +0900

    Add License Text !

--- TWebBrowser/Macapi.WebView.pas
+++ TWebBrowser/Macapi.WebView.pas
@@ -1,3 +1,75 @@
+﻿(*
+ * TWebBrowserEx Classes
+ *   WebBrowser Componet for FireMonkey.
+ *
+ * Copyright (c) 2013, 2014 HOSOKAWA Jun.
+ *
+ * Platform:
+ *   Windows, OS X, iOS, Android
+ *   Delphi / C++Builder XE5, XE6, XE7 and Appmethod 1.14, 1.15
+ *
+ * Contact:
+ *   Twitter @pik or freeonterminate@gmail.com
+ *
+ * Original Source:
+ *   https://github.com/freeonterminate/delphi/tree/master/TWebBrowser
+ *
+ * How to Use:
+ *   1. Add FMX.WebBroserEx to "uses" block.
+ *
+ *   2. Create WebBroserEx and Set parent.
+ *      procedure TForm1.FormCreate(Sender: TObject);
+ *      begin
+ *        FWebBrowser := TWebBrowserEx.Create(Self);
+ *        FWebBrowser.Parent := Panel1;
+ *        FWebBrowser.Align := TAlignLayout.Client;
+ *      end;
+ *
+ *   3. Set URL property.
+ *      procedure TForm1.Button1Click(Sender: TObject);
+ *      begin
+ *        FWebBrowser.URL := 'https://twitter.com/pik';
+ *      end;
+ *
+ * LICENSE:
+ *   本ソフトウェアは「現状のまま」で、明示であるか暗黙であるかを問わず、
+ *   何らの保証もなく提供されます。
+ *   本ソフトウェアの使用によって生じるいかなる損害についても、
+ *   作者は一切の責任を負わないものとします。
+ *
+ *   以下の制限に従う限り、商用アプリケーションを含めて、本ソフトウェアを
+ *   任意の目的に使用し、自由に改変して再頒布することをすべての人に許可します。
+ *
+ *   1. 本ソフトウェアの出自について虚偽の表示をしてはなりません。
+ *      あなたがオリジナルのソフトウェアを作成したと主張してはなりません。
+ *      あなたが本ソフトウェアを製品内で使用する場合、製品の文書に謝辞を入れて
+ *      いただければ幸いですが、必須ではありません。
+ *
+ *   2. ソースを変更した場合は、そのことを明示しなければなりません。
+ *      オリジナルのソフトウェアであるという虚偽の表示をしてはなりません。
+ *
+ *   3. ソースの頒布物から、この表示を削除したり、表示の内容を変更したりしては
+ *      なりません。
+ *
+ *   This software is provided 'as-is', without any express or implied warranty.
+ *   In no event will the authors be held liable for any damages arising from
+ *   the use of this software.
+ *
+ *   Permission is granted to anyone to use this software for any purpose,
+ *   including commercial applications, and to alter it and redistribute
+ *   it freely, subject to the following restrictions:
+ *
+ *   1. The origin of this software must not be misrepresented;
+ *      you must not claim that you wrote the original software.
+ *      If you use this software in a product, an acknowledgment in the product
+ *      documentation would be appreciated but is not required.
+ *
+ *   2. Altered source versions must be plainly marked as such,
+ *      and must not be misrepresented as being the original software.
+ *
+ *   3. This notice may not be removed or altered from any source distribution.
+ *)
+
 unit Macapi.WebView;
 
 {$IF not defined(MACOS) or defined(IOS)}
